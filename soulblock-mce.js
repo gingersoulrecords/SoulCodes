c6e5813f8e235b961cef31116fc19227adc654c7
Ginger Soul Records <gingersoulrecords@gmail.com>
2016-04-07 11:09:57 -0400

    two column nested

--- soulblock-mce.js
+++ soulblock-mce.js
@@ -34,11 +34,11 @@ tinymce.PluginManager.add('soulstyles', function(editor) {
 					editor.execCommand('mceReplaceContent', false, tag+text+tag_end );
 				},
 			},			{
-				text: '[div4]',
+				text: 'Two-Column Nested',
 				onclick: function() {
 				    editor.focus();
-				    var tag 	= '[div4]';
-				    var tag_end = '[/div4]';
+				    var tag 	= '[div1][div2]';
+				    var tag_end = '[/div2][/div1]';
 				    var text = editor.selection.getContent({'format': 'html'});
 					editor.execCommand('mceReplaceContent', false, tag+text+tag_end );
 				},
